98b5ddcadf4343b334c8fb9092c8fa8a2d91a4d6
aminos <amri.amine@protonmail.com>
2019-07-26 17:04:06 +0100

    refactoring

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -1,14 +1,21 @@
-package tacos;
+package tacos.web;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import java.util.Arrays;
+
+import tacos.Ingredient;
+import tacos.Taco;
 import tacos.Ingredient.Type;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +48,13 @@ public class DesignTacoController {
 	    model.addAttribute("design", new Taco());
 	    return "design-a-taco";
 	}
+	
+	@PostMapping
+	public String processDesign(@Valid Taco design) {
+		return "redirect:/orders/current";	
+	}
+	
+	
 
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 		return ingredients
